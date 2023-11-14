2ee451e87da9056737f95dc1ea73ea241f7a653e
Leoryu90 <147222823+Leoryu90@users.noreply.github.com>
2023-11-15 07:49:58 +0900

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,26 @@
 from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options = option)
+
+driver.get('https://kr.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=89395b6ac5014113')
+
+
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+
+if response.status_code != 200:
+    print("Can't requests page")
+
+else:
+    print(response.text)
+
+
+while (True):
+    pass
